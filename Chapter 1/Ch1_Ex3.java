4de57790eb385b7a874272ce189d34eb0b922bd0
Bhuban <71319456+bhuban3@users.noreply.github.com>
2024-11-18 10:21:43 +0600

    Add files via upload

---
+++ Chapter 1/Ch1_Ex3.java
@@ -0,0 +1,8 @@
+public class Ch1_Ex3 {
+    public static void main(String[] args) {
+        //Compute Expression (10.5 + 2 * 3) / (45 - 3.5)
+        System.out.print("(10.5 + 2 * 3) / (45 - 3.5) = ");
+        System.out.println((10.5 + 2 * 3) / (45 - 3.5));
+    }
+
+}
