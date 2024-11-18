3e1aaf09f081b01403dd23c6b450745a08851652
Bhuban <71319456+bhuban3@users.noreply.github.com>
2024-11-18 10:14:03 +0600

    Add files via upload

---
+++ Chapter 1/Ch1_Ex2.java
@@ -0,0 +1,8 @@
+public class Ch1_Ex2 {
+    public static void main(String[] args) {
+        //Welcome With Three Messages
+        System.out.println("Programming is fun!");
+        System.out.println("Fundamentals First");
+        System.out.println("Problem Driven");
+    }
+}
